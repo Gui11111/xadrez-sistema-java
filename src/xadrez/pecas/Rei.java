e1c481dc9a10ad0e3b20c7379f7532c617b05af6
Guilherme <guilhermehenrique14rv@gmail.com>
2022-04-04 21:47:47 -0300

    Implementando os movimentos possíveis do Rei

--- src/xadrez/pecas/Rei.java
+++ src/xadrez/pecas/Rei.java
@@ -1,5 +1,6 @@
 package xadrez.pecas;
 
+import jogoTabuleiro.Posicao;
 import jogoTabuleiro.Tabuleiro;
 import xadrez.Cores;
 import xadrez.PeçaXadrez;
@@ -16,9 +17,67 @@ public class Rei extends PeçaXadrez {
 		return "R";
 	}
 
+	// método que vai falar se o rei pode se mover para uma data posição
+	private boolean podeMover(Posicao posicao) {
+		PeçaXadrez p = (PeçaXadrez) getTabuleiro().peça(posicao);
+		return p == null || p.getCores() != getCores();
+	}
+
+	// testando as posições livres do Rei
 	@Override
 	public boolean[][] movimentosPossiveis() {
 		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+		Posicao p = new Posicao(0, 0);
+
+		// verifcar acima da peça
+		p.setValor(posicao.getLinha() - 1, posicao.getColuna()); // posicao acima do rei
+		if (getTabuleiro().posiçaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// verifcar abaixo da peça
+		p.setValor(posicao.getLinha() + 1, posicao.getColuna()); // posicao abaixo do rei
+		if (getTabuleiro().posiçaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// verifcar esquerda da peça
+		p.setValor(posicao.getLinha(), posicao.getColuna() - 1); // posicao esquerda do rei
+		if (getTabuleiro().posiçaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// verifcar direita da peça
+		p.setValor(posicao.getLinha(), posicao.getColuna() + 1); // posicao direita do rei
+		if (getTabuleiro().posiçaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// verifcar noroeste da peça
+		p.setValor(posicao.getLinha() - 1, posicao.getColuna() - 1); // posicao noroeste do rei
+		if (getTabuleiro().posiçaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// verifcar nordeste da peça
+		p.setValor(posicao.getLinha() - 1, posicao.getColuna() + 1); // posicao nordeste do rei
+		if (getTabuleiro().posiçaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// verifcar sudoeste da peça
+		p.setValor(posicao.getLinha() + 1, posicao.getColuna() - 1); // posicao sudoeste do rei
+		if (getTabuleiro().posiçaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// verifcar sudeste da peça
+		p.setValor(posicao.getLinha() + 1, posicao.getColuna() + 1); // posicao sudeste do rei
+		if (getTabuleiro().posiçaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
 		return mat;
 	}
 }
